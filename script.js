86e659572d48334cfe75062dce49bb985d8081db
luciana <arevalolucianadg@gmail.com>
2019-08-28 10:19:43 -0300

    actualización inicial

---
+++ script.js
@@ -0,0 +1,11 @@
+const app = new Vue ({
+    el: '#app',
+    data: {
+        frutas: [
+            {nombre: 'manzanas', cantidad: 5},
+            {nombre: 'bananas', cantidad: 3},
+            {nombre: 'frutillas', cantidad: 1},
+            {nombre: 'naranjas', cantidad: 0}
+        ]
+    }
+})
